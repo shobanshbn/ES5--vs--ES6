56fd51375c5a25fb6dc8bdac774bde2cba93492b
shobbanshbn <shobanshbn@gmail.com>
2025-06-04 19:30:07 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,31 @@
+//  SPREAD OPERATOR 
+
+// const arr =["guvi","geek","fsd"]
+// console.log(...arr);
+
+/*
+const arr =["bob","alice"]
+const arr1 = ["leo","smith"]
+const arr2 = [...arr,...arr1]
+console.log(arr2);
+*/
+
+// REST PARAMETER
+/*
+function test (...c){
+    let even =[];
+    for(let i=0; i<c.length; i++){
+        if(c[i]%2===0){
+            even.push(c[i])
+        }
+    }
+    return even
+}
+console.log(test(10,11,15,16,14,19,20));
+*/
+
+ 
+
+
+
+
